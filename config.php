e858604520b8ada53c6d9270c62ae45e120628a6
Sayo Paul <yobizzi416@gmail.com>
2017-01-20 07:43:26 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,4 +2,4 @@
 	//set your database comnfiguration here. Ensure to fill in the username and password if there is any.
 	define("DSN","mysql:host=localhost;dbname=test");
 	define("USER","root");
-	define("PASS","jesusisLord");
+	define("PASS","");
